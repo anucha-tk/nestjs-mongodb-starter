d24032f11db0ee0b4397bac668d3c8b7a871bd59
Anucha.tk <anuchataowkaen@gmail.com>
2023-06-07 17:24:28 +0700

    feat(etc): little code

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,7 @@
 import AppConfig from "./app.config";
 import DatabaseConfig from "./database.config";
 import DocConfig from "./doc.config";
+import RequestConfig from "./request.config";
+import MessageConfig from "./message.config";
 
-export default [AppConfig, DatabaseConfig, DocConfig];
+export default [AppConfig, DatabaseConfig, DocConfig, RequestConfig, MessageConfig];
