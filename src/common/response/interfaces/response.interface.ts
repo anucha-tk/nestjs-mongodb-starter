b79a9dd8c1613723935393e7afc8a19b44e1feac
Anucha.tk <anuchataowkaen@gmail.com>
2023-06-09 11:29:33 +0700

    feat(response): add reponse module

---
+++ src/common/response/interfaces/response.interface.ts
@@ -0,0 +1,41 @@
+import { HttpStatus } from "@nestjs/common";
+import { ClassConstructor } from "class-transformer";
+import { IMessageOptionsProperties } from "src/common/message/interfaces/message.interface";
+
+export interface IResponseCustomPropertyMetadata {
+  statusCode?: number;
+  message?: string;
+  httpStatus?: HttpStatus;
+  messageProperties?: IMessageOptionsProperties;
+}
+
+// metadata
+export interface IResponseMetadata {
+  customProperty?: IResponseCustomPropertyMetadata;
+  [key: string]: any;
+}
+
+// type
+export interface IResponse {
+  _metadata?: IResponseMetadata;
+  data?: Record<string, any>;
+}
+
+// decorator options
+export interface IResponseOptions<T> {
+  serialization?: ClassConstructor<T>;
+  messageProperties?: IMessageOptionsProperties;
+}
+
+export type IResponsePagingOptions<T> = IResponseOptions<T>;
+
+export interface IResponsePagingPagination {
+  totalPage: number;
+  total: number;
+}
+
+export interface IResponsePaging {
+  _metadata?: IResponseMetadata;
+  _pagination: IResponsePagingPagination;
+  data: Record<string, any>[];
+}
---
+++ test/e2e/response/response.paging.e2e-spec.ts
@@ -0,0 +1,5 @@
+describe("paging", () => {
+  it.skip("should return paging property response", () => {
+    // TODO: write test paging response
+  });
+});
