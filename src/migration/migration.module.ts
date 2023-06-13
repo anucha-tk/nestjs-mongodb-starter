74919b1fe6c0eaf8ed46034883b613cd3aad3d8f
Anucha.tk <anuchataowkaen@gmail.com>
2023-06-14 00:18:18 +0700

    feat(seed): apikey seed with nestjs-command

---
+++ src/migration/migration.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { CommandModule } from "nestjs-command";
+import { ApiKeyModule } from "src/common/api-key/api-key.module";
+import { CommonModule } from "src/common/common.module";
+import { MigrationApiKeySeed } from "src/migration/seeds/migration.api-key.seed";
+
+@Module({
+  imports: [CommonModule, CommandModule, ApiKeyModule],
+  providers: [MigrationApiKeySeed],
+  exports: [],
+})
+export class MigrationModule {}
